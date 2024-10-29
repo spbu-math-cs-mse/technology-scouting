0f9e0792f5183d383bba7ffd78cb3b37e884ecf0
FedorKhokhryakov <fekhokh@gmail.com>
2024-10-29 21:13:37 +0300

    connect bot with DB

--- src/main/kotlin/com/technology_scouting/Main.kt
+++ src/main/kotlin/com/technology_scouting/Main.kt
@@ -8,28 +8,28 @@ import com.github.kotlintelegrambot.entities.*
 private val BOT_TOKEN = System.getenv("BOT_TOKEN")
 
 fun main() {
-    /*val dbService = DatabaseService()
+    val dbService = DatabaseService()
     val userService = UserService(dbService.database)
 
     val users = userService.getUserRecords()
+    users.forEach { println(it) }
 
-    dbService.closeConnection()*/
     val bot = bot {
         token = BOT_TOKEN
         dispatch {
             text {
                 var Message = ChatId.fromId(message.chat.id) // сюда запоминается смс-ка
-                bot.sendMessage( // это он просто пока пишет на экран то, что ты ввел(просто понимать
+                /*bot.sendMessage( // это он просто пока пишет на экран то, что ты ввел(просто понимать
                     // работает бот или нет)
                     chatId = Message,
                     messageThreadId = message.messageThreadId,
                     text = text,
                     protectContent = true,
                     disableNotification = false,
-                )
+                )*/
             }
             command("start") {
-                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Hi there!")
+                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Hi!")
                 result.fold({
                     // do something here with the response
                 },{
@@ -37,9 +37,21 @@ fun main() {
                 })
             }
             command("help") {
-                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "иди нахуй")
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "При помоши команды /enquire вы можете отправить свой запрос в базу данных.\n" +
+                        "Для этого напишите свой текст в одном сообщении после данной команды.")
+            }
+            command("enquire") {
+                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Записал ваше сообщение")
+                result.fold({
+                    val userId = message.chat.username.toString()
+                    val userMessage = message.text!!.substring(8, message.text!!.length).trim()
+                    userService.addUserRecord(userId, userMessage)
+                },{
+                    // do something with the error
+                })
             }
         }
     }
     bot.startPolling()
+    //dbService.closeConnection()
 }
