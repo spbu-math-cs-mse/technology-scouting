52a84647dd9e1129cd4167611dd0f19703de9565
katyadoinikova <k.doinikova@yandex.ru>
2024-10-30 09:43:11 +0300

    without test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
 test("dummy id test", () => {
   "a" === "a"
-=======
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
->>>>>>> 16852d4 (style: apply frontend formatter)
 });
