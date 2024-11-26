149a985945f13f51a92886ac6d1f81434041d360
katyadoinikova <k.doinikova@yandex.ru>
2024-11-27 02:47:43 +0300

    fix: popup editted

--- frontend/src/components/ResourceEditDialog.tsx
+++ frontend/src/components/ResourceEditDialog.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Grid from "@mui/material/Grid2";
+import Grid from "@mui/material/Grid";
 import {
   Dialog,
   DialogActions,
@@ -7,8 +7,12 @@ import {
   DialogTitle,
   TextField,
   Button,
+  MenuItem,
+  MenuList,
+  Popover,
 } from "@mui/material";
-import { Resource } from "../logic/types";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {Resource} from "../logic/types"
 
 type ResourceEditDialogProps = {
   open: boolean;
@@ -24,6 +28,8 @@ export default function ResourceEditDialog({
   editResource,
 }: ResourceEditDialogProps) {
   const [editedState, setEditedState] = useState(initialState);
+  const [statusPopoverAnchor, setStatusPopoverAnchor] = useState<null | HTMLElement>(null);
+  const [status, setStatus] = useState(initialState.status);
 
   const handleChange = (
     key: string,
@@ -37,25 +43,72 @@ export default function ResourceEditDialog({
     setOpen(false);
   };
 
+  const handleStatusChange = (newStatus: string) => {
+    setStatus(newStatus);
+    setEditedState({ ...editedState, status: newStatus });
+    setStatusPopoverAnchor(null); // Закрыть поповер после изменения статуса
+  };
+
+  const handleOpenPopover = (event: React.MouseEvent<HTMLElement>) => {
+    setStatusPopoverAnchor(event.currentTarget);
+  };
+
+  const handleClosePopover = () => {
+    setStatusPopoverAnchor(null);
+  };
+
+  const statusOptions = ["Pending", "Approved", "Rejected", "In Progress"];
+
   return (
     <Dialog open={open}>
       <DialogTitle>Edit Information</DialogTitle>
       <DialogContent>
         <Grid container spacing={2}>
-          {(Object.keys(initialState) as Array<keyof Resource>).map(
-            (key) => (
-              <Grid size={{ xs: 6 }} key={key}>
-                <TextField
-                  name={key}
-                  label={key.charAt(0).toUpperCase() + key.slice(1)}
-                  value={editedState[key]}
-                  onChange={(event) => handleChange(key, event)}
-                  fullWidth
-                  margin="normal"
-                />
-              </Grid>
-            )
-          )}
+          {(Object.keys(initialState) as Array<keyof Resource>).map((key) => (
+            <Grid item xs={6} key={key}>
+              <TextField
+                name={key}
+                label={key.charAt(0).toUpperCase() + key.slice(1)}
+                value={editedState[key]}
+                onChange={(event) => handleChange(key, event)}
+                fullWidth
+                margin="normal"
+              />
+            </Grid>
+          ))}
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            aria-label="status"
+            onClick={handleOpenPopover}
+            size="small"
+            color="primary"
+            startIcon={<ExpandMoreIcon />}
+          >
+            Change Status (Current: {status})
+          </Button>
+
+          <Popover
+            open={Boolean(statusPopoverAnchor)}
+            anchorEl={statusPopoverAnchor}
+            onClose={handleClosePopover}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <MenuList>
+              {statusOptions.map((option) => (
+                <MenuItem key={option} onClick={() => handleStatusChange(option)}>
+                  {option}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Popover>
         </Grid>
       </DialogContent>
       <DialogActions>
--- frontend/src/components/ResourceTable.tsx
+++ frontend/src/components/ResourceTable.tsx
@@ -34,8 +34,6 @@ export default function ResourceTable() {
   const [selectedForDeleteRequestId, setSelectedForDeleteRequestId] = useState<
     string | null
   >(null);
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [activeRow, setActiveRow] = useState<string | null>(null);
 
   const handleOpenDialogForDelete = (id: string) => {
     setSelectedForDeleteRequestId(id);
@@ -50,29 +48,6 @@ export default function ResourceTable() {
     getResourcesDataTable().then((messages) => setTableContent(messages));
   };
 
-  const handleEditStatus = (id: string, new_status: string) => {
-    postEditApplication(id, new_status);
-    getResourcesDataTable().then((messages) => setTableContent(messages));
-    handleClosePopover();
-  };
-
-  const handleOpenPopover = (
-    event: React.MouseEvent<HTMLElement>,
-    rowIndex: string
-  ) => {
-    setAnchorEl(event.currentTarget);
-    setActiveRow(rowIndex);
-  };
-
-  const handleClosePopover = () => {
-    setAnchorEl(null);
-    setActiveRow(null);
-  };
-
-  const statusOptions = ["Pending", "Approved", "Rejected", "In Progress"];
-
-  const isPopoverOpen = Boolean(anchorEl);
-
   useEffect(() => {
     getResourcesDataTableMock().then((messages) => setTableContent(messages));
     const interval = setInterval(() => {
@@ -83,54 +58,48 @@ export default function ResourceTable() {
     };
   }, []);
 
-  const [resourceEditDialogOpen, setResourceEditDialogOpen] =
-    useState(false);
-  const [editingResource, setEditingResource] = useState<
-    Resource | undefined
-  >(undefined);
+  const [resourceEditDialogOpen, setResourceEditDialogOpen] = useState(false);
+  const [editingResource, setEditingResource] = useState<Resource | undefined>(
+    undefined
+  );
 
   return (
     <>
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell>Organization</TableCell>
-            <TableCell>ContactName</TableCell>
-            <TableCell>TelegramId</TableCell>
-            <TableCell>CompetenceField</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Tags</TableCell>
-            <TableCell>Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableContent.map((resourceMessage, ind) => (
-            <TableRow key={ind}>
-              <TableCell>{resourceMessage._id}</TableCell>
-              <TableCell>{resourceMessage.date}</TableCell>
-              <TableCell>{resourceMessage.organization}</TableCell>
-              <TableCell>{resourceMessage.contactName}</TableCell>
-              <TableCell>{resourceMessage.telegramId}</TableCell>
-              <TableCell>{resourceMessage.competenceField}</TableCell>
-              <TableCell>{resourceMessage.description}</TableCell>
-              <TableCell>{resourceMessage.tags.join(", ")}</TableCell>
-              <TableCell>
-                  {resourceMessage.status}
-                  <IconButton
-                    onClick={(e) => handleOpenPopover(e, resourceMessage._id)}
-                  >
-                    <ExpandMoreIcon />
-                  </IconButton>
-                </TableCell>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Organization</TableCell>
+              <TableCell>ContactName</TableCell>
+              <TableCell>TelegramId</TableCell>
+              <TableCell>CompetenceField</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Tags</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableContent.map((resourceMessage, ind) => (
+              <TableRow key={ind}>
+                <TableCell>{resourceMessage._id}</TableCell>
+                <TableCell>{resourceMessage.date}</TableCell>
+                <TableCell>{resourceMessage.organization}</TableCell>
+                <TableCell>{resourceMessage.contactName}</TableCell>
+                <TableCell>{resourceMessage.telegramId}</TableCell>
+                <TableCell>{resourceMessage.competenceField}</TableCell>
+                <TableCell>{resourceMessage.description}</TableCell>
+                <TableCell>{resourceMessage.tags.join(", ")}</TableCell>
+                <TableCell>{resourceMessage.status}</TableCell>
                 <TableCell>
                   <IconButton
                     aria-label="delete"
                     size="large"
                     color="error"
-                    onClick={() => handleOpenDialogForDelete(resourceMessage._id)}
+                    onClick={() =>
+                      handleOpenDialogForDelete(resourceMessage._id)
+                    }
                   >
                     <DeleteIcon />
                   </IconButton>
@@ -178,43 +147,6 @@ export default function ResourceTable() {
             ))}
           </TableBody>
         </Table>
-        <Popover
-          open={isPopoverOpen}
-          anchorEl={anchorEl}
-          onClose={handleClosePopover}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "center",
-          }}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "center",
-          }}
-        >
-          <MenuList>
-            {statusOptions.map((status) => (
-              <MenuItem
-                key={status}
-                onClick={() =>
-                  activeRow !== null &&
-                  handleEditStatus(String(activeRow), status)
-                }
-                sx={{
-                  fontSize: "11px",
-                  backgroundColor: "white",
-                  "&:hover": {
-                    backgroundColor: "#ADD8E6",
-                  },
-                  borderRadius: "8px",
-                  padding: "12px",
-                  color: "#333",
-                }}
-              >
-                {status}
-              </MenuItem>
-            ))}
-          </MenuList>
-        </Popover>
       </TableContainer>
       {editingResource ? (
         <ResourceEditDialog
@@ -229,4 +161,3 @@ export default function ResourceTable() {
     </>
   );
 }
-
